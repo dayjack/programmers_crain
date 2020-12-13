7cbd314e698bb039ddf936bf8cbb9d5a3fd58498
dayjack <frostjanna12@gmail.com>
2020-12-13 16:09:43 +0900

    프로그래머스 문제 풀이 공부

--- programmers_crain/main.swift
+++ programmers_crain/main.swift
@@ -7,5 +7,39 @@
 
 import Foundation
 
-print("Hello, World!")
+let n1 = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+let n2 = [1,5,3,5,1,2,1,4]
+
+func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
+    var box = [Int]()
+    var fboard = board
+    var fmoves = moves
+    var result: Int = 0
+    NSLog("board[0].count: \(board[0].count)")
+    while !fmoves.isEmpty {
+        
+        for i in 0..<board[0].count {
+            
+            NSLog("\ni = \(i)\nfmoves count: \(fmoves.count)")
+            
+            if fboard[board[0].count - 1][fmoves[0]-1] == 0 {
+                fmoves.removeFirst()
+                break
+            } else if fboard[i][fmoves[0]-1] != 0 {
+                if fboard[i][fmoves[0]-1] == box.last {
+                    box.removeLast()
+                    result += 2
+                } else {
+                    box.append(fboard[i][fmoves[0]-1])
+                }
+                fboard[i][fmoves[0]-1] = 0
+                fmoves.removeFirst()
+                break
+            }
+        }
+    }
+    return result
+}
+
+print(solution(n1, n2))
 
